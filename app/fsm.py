f822eab6a86c9d6444d7bc99f638259b231eb0d0
harshitaukv <146750492+harshitaukv@users.noreply.github.com>
2025-12-14 12:25:18 +0530

    Update fsm.py

--- app/fsm.py
+++ app/fsm.py
@@ -1,1 +1,16 @@
+class ReturnFSM:
+    def __init__(self):
+        self.state = "ORDER_DELIVERED"
+
+    def next(self, action):
+        transitions = {
+            "ORDER_DELIVERED": ["REQUEST_RETURN"],
+            "REQUEST_RETURN": ["PICKUP_SCHEDULED"],
+            "PICKUP_SCHEDULED": ["REFUND_INITIATED"],
+            "REFUND_INITIATED": ["REFUND_COMPLETED"]
+        }
+
+        if action in transitions.get(self.state, []):
+            self.state = action
+        return self.state
 
