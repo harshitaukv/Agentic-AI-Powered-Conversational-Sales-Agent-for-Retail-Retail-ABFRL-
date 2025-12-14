bb183921374674666c68d77b9dc9da5026234e47
harshitaukv <146750492+harshitaukv@users.noreply.github.com>
2025-12-14 12:13:44 +0530

    Update rag.py

--- app/rag.py
+++ app/rag.py
@@ -1,1 +1,9 @@
+from app.data import PRODUCTS
+
+def search_products(query: str):
+    results = []
+    for p in PRODUCTS:
+        if query.lower() in p["name"].lower() or query.lower() in p["category"].lower():
+            results.append(p)
+    return results
 
