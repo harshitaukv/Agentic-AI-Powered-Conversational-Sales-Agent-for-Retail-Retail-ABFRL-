d47be0330d434ee6d3dc55065d2b94449b1db658
harshitaukv <146750492+harshitaukv@users.noreply.github.com>
2025-12-14 12:23:12 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from app.models import ChatRequest, ChatResponse
-
+from app.analytics import get_analytics
 @app.post("/chat", response_model=ChatResponse)
 def chat(req: ChatRequest):
     reply = route_intent(req.message)
     return {"response": reply}
+@app.get("/analytics")
+def analytics():
+    return get_analytics()
 
 #Run using
 #uvicorn app.main:app --reload
